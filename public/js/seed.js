1289ea5af872ec20fce1932e09ac2453371a7a4f
Brian Bycynski <high5bri@hotmail.com>
2017-02-27 09:08:27 -0500

    finished

--- public/js/seed.js
+++ public/js/seed.js
@@ -6,7 +6,7 @@ window.Seed = (function () {
   const products = [
     {
       id: 1,
-      title: 'Yellow Pail',
+      title: 'Yellow Pailssss',
       description: 'On-demand sand castle construction expertise.',
       url: '#',
       votes: generateVoteCount(),
